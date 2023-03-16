0e1d5540b6bbb5e0bd4c2786b00a2b7331615c3c
melvinkukwansafari <melvin.kukwan@student.moringaschool.com>
2023-03-16 20:49:00 +0300

    complete code along

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,5 +1,7 @@
 class BirdsController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+   # added rescue_from
+   rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
 
   # GET /birds
   def index
@@ -9,6 +11,7 @@ class BirdsController < ApplicationController
 
   # POST /birds
   def create
+    # create! exceptions will be handled by the rescue_from ActiveRecord::RecordInvalid code
     bird = Bird.create(bird_params)
     render json: bird, status: :created
   end
@@ -22,10 +25,11 @@ class BirdsController < ApplicationController
   # PATCH /birds/:id
   def update
     bird = find_bird
-    bird.update(bird_params)
+    # update! exceptions will be handled by the rescue_from ActiveRecord::RecordInvalid code
+    bird.update!(bird_params)
     render json: bird
+  
   end
-
   # DELETE /birds/:id
   def destroy
     bird = find_bird
@@ -34,6 +38,10 @@ class BirdsController < ApplicationController
   end
 
   private
+
+  def render_unprocessable_entity_response(invalid)
+    render json: { errors: invalid.record.errors }, status: :unprocessable_entity
+  end
 
   def find_bird
     Bird.find(params[:id])
